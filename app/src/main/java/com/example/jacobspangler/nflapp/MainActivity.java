c0b4a8706e423d63b1a2fe7f550f98f79d5b587b
jspangler8 <jacobrs4@illinois.edu>
2018-12-02 14:07:26 -0600

    setup app

--- app/src/main/java/com/example/jacobspangler/nflapp/MainActivity.java
+++ app/src/main/java/com/example/jacobspangler/nflapp/MainActivity.java
@@ -2,12 +2,101 @@ package com.example.jacobspangler.nflapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Main class for our UI design lab.
+ */
+public final class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Lab11:Main";
+
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
+    /**
+     * Run when this activity comes to the foreground.
+     *
+     * @param savedInstanceState unused
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up the queue for our API requests
+        requestQueue = Volley.newRequestQueue(this);
+
         setContentView(R.layout.activity_main);
+        final android.widget.Button click = findViewById(R.id.button);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                startAPICall("192.17.96.8");
+            }
+        });
+    }
+
+    /**
+     * Run when this activity is no longer visible.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * Make a call to the IP geolocation API.
+     *
+     * @param ipAddress IP address to look up
+     */
+    void startAPICall(final String ipAddress) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://ipinfo.io/" + ipAddress + "/json",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            apiCallDone(response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            jsonObjectRequest.setShouldCache(false);
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handle the response from our IP geolocation API.
+     *
+     * @param response response from our IP geolocation API.
+     */
+    void apiCallDone(final JSONObject response) {
+        try {
+            Log.d(TAG, response.toString(2));
+            // Example of how to pull a field off the returned JSON object
+            Log.i(TAG, response.get("hostname").toString());
+            final android.widget.TextView location = findViewById(R.id.textView);
+            String town = response.get("city").toString();
+            location.setText(town);
+        } catch (JSONException ignored) { }
     }
 }
